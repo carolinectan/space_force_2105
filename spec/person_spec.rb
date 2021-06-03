1654461e4f9d4e805990c7d7335bf82a11a39aac
Caroline Tan <carolinectan3@gmail.com>
2021-06-03 11:07:46 -0400

    Add person tests

---
+++ spec/person_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require './lib/person'
+
+
+RSpec.describe Person do
+  before :each do
+    @kathy = Person.new('Kathy Chan', 10)
+  end
+
+  it 'exists' do
+    expect(@kathy).to be_a(Person)
+  end
+
+  it 'initiates with attributes' do
+    expect(@kathy.name).to eq('Kathy Chan')
+    expect(@kathy.experience).to eq(10)
+    expect(@kathy.specialties).to eq([])
+  end
+
+  it 'can add specialties' do
+    @kathy.add_specialty(:astrophysics)
+    @kathy.add_specialty(:quantum_mechanics)
+
+    expect(@kathy.specialties).to eq([:astrophysics, :quantum_mechanics])
+  end
+end
